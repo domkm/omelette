c2ca944c94fd40d6f7d807523e79c1645f36578c
Dom Kiva-Meyer <git@domkm.com>
2014-06-11 19:10:19 -0700

    refactor and add doc string

--- src/omelette/serve.clj
+++ src/omelette/serve.clj
@@ -3,30 +3,34 @@
             [compojure.core :as compojure]
             [compojure.handler :as handler]
             [org.httpkit.server :as http-kit]
-            [ring.middleware.anti-forgery :as ring-anti-forgery]))
+            [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]))
 
 (defrecord Server [port]
   component/Lifecycle
   (start
    [component]
-   (if (:server component)
+   (if (:stop! component)
      component
      (let [server
            (-> component
                :router
                :ring-routes
-               (ring-anti-forgery/wrap-anti-forgery {:read-token #(-> % :params :csrf-token)})
+               (wrap-anti-forgery {:read-token (comp :csrf-token :params)})
                handler/site
                (http-kit/run-server {:port (or port 0)}))
            port
            (-> server meta :local-port)]
        (println "Web server running on port " port)
-       (assoc component :server server :port port))))
+       (assoc component :stop! server :port port))))
   (stop
    [component]
-   (when-let [server (:server component)]
-     (server :timeout 250))
-   (dissoc component :server :router)))
+   (when-let [stop! (:stop! component)]
+     (stop! :timeout 250))
+   (dissoc component :stop! :router)))
 
-(defn server []
-  (map->Server {}))
+(defn server
+  "Takes a port number.
+  Returns an http-kit server component.
+  Requires `(get-in this [:router :ring-routes])` to be a routes."
+  [port]
+  (map->Server {:port port}))
